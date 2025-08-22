51c23dc5e9f8a75c58b7007ab27f1315b6211152
aditii-15 <aditimishra30527@gmail.com>
2025-08-22 21:38:37 +0530

    Add Files

---
+++ 0_variables.js
@@ -0,0 +1,24 @@
+// console.log("hello world")
+// console.log("code is running....")
+// console.log("code is also running......")
+// console.log("code is looking like a wow")
+
+//* console works in the node which is in the vs code 
+
+// var myAge = 20;
+// console.log(myAge);
+
+
+//! Questions below
+
+var my_firstName = "Aditi";
+console.log(my_firstName);  // valid
+
+var _myLastName$ = "Mishra";
+console.log(_myLastName$);  // valid
+
+// var 123myAge = 20;
+// console.log(123myAge); // not valid
+
+var $cityName = "New Delhi";
+console.log($cityName); // valid
---
+++ windows/script.js
@@ -0,0 +1,1 @@
+console.log('Its an external file in js')
