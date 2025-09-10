4d3ef59c7aed07e3f3eb4a48fbdeb8acfe5689b1
aditii-15 <aditimishra30527@gmail.com>
2025-09-10 18:56:09 +0530

    Files

---
+++ windows/bom/index.js
@@ -0,0 +1,9 @@
+//* Types of Nodes in Js.
+
+//? Element node: An HTML tag, the tree building blocks
+
+//? Text node: In the DOM tree, text content, including new lines, spaces, and tabs, is treated as text nodes.
+
+//? Attribute node: An attribute of an element
+
+//? Comment node: Represents comments within the HTML document
