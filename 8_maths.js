340da7eed67a84aeab8d36fff1406a236d4cec19
aditii-15 <aditimishra30527@gmail.com>
2025-09-10 15:48:14 +0530

    Files

---
+++ 8_maths.js
@@ -0,0 +1,16 @@
+//? Constants:
+
+const piValue = Math.PI;
+console.log(piValue);
+
+// todo Math.trunc and Math.sign
+const truncValue = Math.trunc(-3.7); // Output - (-3)
+const floorValue = Math.floor(-3.7); // Output - (-4)
+console.log(truncValue);
+console.log(floorValue);
+
+//? Exponentional and Logarithm
+
+// Math.pow(x, y) : Return the value of x to the power y
+console.log(Math.pow(3, 4));
+console.log(3 ** 4);
